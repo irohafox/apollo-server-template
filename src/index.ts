4a404591258086dd211de499df4d702ab1857172
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-12-10 10:37:54 +0900

    Chore: dotenv導入

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import express from 'express'
 import http from 'http'
 
+require('dotenv').config()
+
 const typeDefs = gql`
   type Query {
     me: User
@@ -33,14 +35,19 @@ async function listen() {
   server.applyMiddleware({ app })
 
   return new Promise((resolve, reject) => {
-    httpServer.listen(4000).once('listening', resolve).once('error', reject)
+    httpServer
+      .listen(process.env.HTTP_SERVER_PORT)
+      .once('listening', resolve)
+      .once('error', reject)
   })
 }
 
 async function main() {
   try {
     await listen()
-    console.log('Server is ready at http://localhost:4000/graphql')
+    console.log(
+      `Server is ready at http://localhost:${process.env.HTTP_SERVER_PORT}/graphql`
+    )
   } catch (err) {
     console.error('Error starting the node server', err)
   }
