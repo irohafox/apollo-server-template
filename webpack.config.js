cb60e0228a212af5833f3d93eddccd2d3a428d3f
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-12-10 12:47:28 +0900

    WIP: webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        loader: 'ts-loader',
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        options: {
+          configFile: 'tsconfig.json'
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src')
+    },
+    extensions: ['.ts', '.js', 'json']
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
