a8c2826bf90b4ed39dad00df627fd740e2e51953
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-12-14 14:03:35 +0900

    Refac: seedを雛形だけにしておく

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,19 +2,7 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 const main = async () => {
-  const users = await createUsers()
-  console.log(users)
-}
-
-const createUsers = async () => {
-  const promises = [...Array(3)].map((_, i) => {
-    return prisma.user.create({
-      data: {
-        name: `seed_user_${i + 1}`
-      }
-    })
-  })
-  return await Promise.all(promises)
+  console.log('Seeding finished.')
 }
 
 main()
