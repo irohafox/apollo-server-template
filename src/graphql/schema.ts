0a7d9289e3dbb4eb2028b92ff08e3171f2118ebc
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-12-14 14:38:40 +0900

    WIP: ちょっとやり方わかってきた

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,9 @@
 import { join } from 'path'
 import { readdirSync, readFileSync } from 'fs'
 import { makeExecutableSchema } from '@graphql-tools/schema'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const gqlFiles = readdirSync(join(__dirname, './typedefs'))
 
@@ -13,7 +16,16 @@ const typeDefs = gqlFiles.map((file) => {
 // TODO: resolversの分割
 const resolvers = {
   Query: {
-    getUser: () => ({ name: 'irohafox' })
+    user: (_: any, { id }: { id: number }) => {
+      return prisma.user.findUnique({
+        where: {
+          id: id
+        }
+      })
+    },
+    users: () => {
+      return prisma.user.findMany()
+    }
   }
 }
 
