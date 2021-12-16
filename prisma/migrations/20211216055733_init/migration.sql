2e7dc78cd506e29ccdb5b0a70e9664622896d5a7
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-12-16 14:58:07 +0900

    WIP: 認証に向けusersテーブルを調整

--- prisma/migrations/20211216055733_init/migration.sql
+++ prisma/migrations/20211216055733_init/migration.sql
@@ -1,9 +1,12 @@
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `encrypted_password` VARCHAR(191) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
 
+    UNIQUE INDEX `users_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
