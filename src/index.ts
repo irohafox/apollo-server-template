17b9c950b5a866d2d464e195c44350447d25fb7a
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-12-23 09:11:14 +0900

    Chore: authをサービス層に

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { ApolloServer } from 'apollo-server-express'
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core'
 import express from 'express'
 import http from 'http'
-import authRouter from '@src/auth'
+import authRouter from '@src/services/auth'
 import schema from '@src/services/graphql/schema'
 
 require('dotenv').config()
