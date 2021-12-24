7aa8d21461ab94a51da852d5562e80da029fc341
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-12-24 14:17:42 +0900

    WIP: contextの確認

--- src/services/graphql/resolvers/user.ts
+++ src/services/graphql/resolvers/user.ts
@@ -2,14 +2,16 @@ import { prisma } from '@src/middleware/db'
 
 const userResolver = {
   queries: {
-    user: (_: any, { id }: { id: number }) => {
+    user: (_: any, { id }: { id: number }, context: any) => {
+      console.log(context.currentUser)
       return prisma.user.findUnique({
         where: {
           id: id
         }
       })
     },
-    users: () => {
+    users: (_: any, {}, context: any) => {
+      console.log(context.currentUser)
       return prisma.user.findMany()
     }
   }
